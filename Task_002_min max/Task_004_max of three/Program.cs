e4c5b20e97d8deac04777ac43fcf77acc924ad9f
Andrew <and_0@bk.ru>
2022-02-02 20:05:48 +0400

    Сделал задачу 4: Найти максимальное из трех чисел

---
+++ Task_002_min max/Task_004_max of three/Program.cs
@@ -0,0 +1,29 @@
+﻿// Найти максимальное из трех чисел
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+int max;
+int min=a;
+Console.WriteLine(a + " " + b + " " + c);
+
+if (a == b && b == c)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (a >= b && a >= c)
+{
+    max = a;
+    Console.WriteLine("Max number: " + max);
+}
+else if (b >= a && b > c)
+{
+    max = b;
+    Console.WriteLine("Max number: " + max);
+}
+else if (c > a && c >= b)
+{
+    max = c;
+    Console.WriteLine("Max number: " + max);
+}
+
+
