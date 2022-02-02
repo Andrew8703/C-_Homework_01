0613350835f998a257f0b603a92a05cb5f27c383
Andrew <and_0@bk.ru>
2022-02-02 19:13:41 +0400

    Сделал задачу 2: даны два числа. оказать большее и меньшее число

---
+++ Task_002_min max/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int a = new Random().Next(1,100);
+int b = new Random().Next(1,100);
+int max;
+int min;
+Console.WriteLine("number1: " +a);
+Console.WriteLine("number2: " +b);
+if (a>b)
+{
+    min=b;
+    max=a;
+    Console.WriteLine("Min = " + min + "  Max = " + max);
+} 
+else if (a<b)
+{
+    min=a;
+    max=b;
+    Console.WriteLine("Min = " + min + "  Max = " + max);
+}
+else
+Console.WriteLine("Числа равны");
